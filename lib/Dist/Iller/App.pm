ea62eb513f840af2291c731fe000f40e70abb06d
Erik Carlsson <info@code301.com>
2015-02-01 22:59:45 +0100

    Making it into a yaml thing

--- lib/Dist/Iller/App.pm
+++ lib/Dist/Iller/App.pm
@@ -2,7 +2,6 @@ package Dist::Iller::App;
 
 # VERSION
 
-use 5.20.0;
 use strict;
 use warnings;
 use experimental 'postderef';
@@ -51,46 +50,6 @@ sub execute_command {
     my $self = shift;
     $self->SUPER::execute_command(@_);
 
-}
-
-sub make_dist_ini {
-    my @plugins_to_remove = @_;
-
-    my $out = "; This file has been auto-generated\n\n";
-    my $iller = Config::INI::Reader->read_file('iller.ini');
-
-    foreach my $headerkey (sort keys $iller->{'_'}->%*) {
-        $out .= sprintf "%s = %s\n", $headerkey, $iller->{'_'}{ $headerkey };
-    }
-    $out .= "\n";
-    delete $iller->{'_'};
-
-    PLUGIN:
-    foreach my $plugin (sort keys $iller->%*) {
-        next PLUGIN if any { $plugin eq $_ } @plugins_to_remove;
-
-        # Bundle?
-        if($plugin =~ m{^@}) {
-            my $settings = [ map { $_ => $iller->{ $plugin }{ $_ } } keys $iller->{ $plugin }->%* ];
-            my $bundle = Dist::Zilla::Util::BundleInfo->new(bundle_name => $plugin, bundle_payload => $settings);
-
-            foreach my $plugin_in_bundle ($bundle->plugins) {
-                $out .= $plugin_in_bundle->to_dist_ini;
-                $out .= "\n";
-            }
-        }
-        else {
-            $out .= sprintf "[%s]\n", $plugin;
-
-            foreach my $setting (sort keys $iller->{ $plugin }->%*) {
-                $out .= sprintf "%s = %s" => $setting, $iller->{ $plugin }{ $setting };
-            }
-            $out .= "\n";
-        }
-    }
-
-    path('dist.ini')->touch->spew_utf8($out);
-            warn '   Has generated dist.ini';
 }
 
 1;

ed81adb84467547896c4ff575ea5170f321f8d4b
Erik Carlsson <info@code301.com>
2016-02-05 20:06:35 +0000

    Remove unused code

--- lib/Dist/Iller/Plugin.pm
+++ lib/Dist/Iller/Plugin.pm
@@ -69,45 +69,6 @@ sub merge_with {
     }
 }
 
-sub plugin_package {
-    my $self = shift;
-    my $doctype = shift;
-
-    my @packages = ();
-    # For -Transformer
-    if($doctype->is_weaver && $self->has_in && $self->has_base) {
-        if($self->in eq 'Elemental') {
-            my $base = $self->base;
-            $base =~ s{^[^a-zA-Z]}{};
-
-            push @packages => join '::' => 'Pod::Elemental', $base, $self->plugin_name;
-            $self->in('Plugin'); # eg. Pod::Elemental::Transformer -> Pod::Weaver::Plugin::Transformer (a bit messy)
-        }
-    }
-    my $name = $self->has_base ? $self->base : $self->plugin_name;
-    $name =~ m{^(.)};
-    my $first = $1;
-
-    my $clean_name = $name;
-    $clean_name =~ s{^[-%=@]}{};
-
-    if($doctype->is_dist) {
-        push @packages => $first eq '%' ? sprintf '%s::%s::%s' => $doctype->namespace, 'Stash', $clean_name
-                       :  $first eq '@' ? sprintf '%s::%s::%s' => $doctype->namespace, 'PluginBundle', $clean_name
-                       :  $first eq '=' ? sprintf $clean_name
-                       :                  sprintf '%s::%s::%s' => $doctype->namespace, 'Plugin', $clean_name
-                       ;
-    }
-    elsif($doctype->is_weaver) {
-        push @packages => $first eq '-' ? sprintf '%s::%s::%s' => $doctype->namespace, 'Plugin', $clean_name
-                       :  $first eq '@' ? sprintf '%s::%s::%s' => $doctype->namespace, 'PluginBundle', $clean_name
-                       :  $first eq '=' ? sprintf $clean_name
-                       :                  sprintf '%s::%s::%s' => $doctype->namespace, 'Section', $clean_name
-                       ;
-    }
-    return @packages;
-}
-
 sub to_string {
     my $self = shift;
     my @strings = $self->has_base ? (sprintf '[%s / %s]' => $self->base, $self->plugin_name)

474ba03d74d19aa6e7713fb99b20f5dfbc782d4d
Erik Carlsson <info@code301.com>
2020-12-27 13:12:05 +0000

    Add support in doctype:Dist to set header.name to global:distribution_name

--- lib/Dist/Iller/DocType/Dist.pm
+++ lib/Dist/Iller/DocType/Dist.pm
@@ -19,11 +19,13 @@ use Types::Standard qw/HashRef ArrayRef Str Int Bool/;
 use PerlX::Maybe qw/maybe provided/;
 use List::Util qw/any/;
 
+
 has name => (
     is => 'rw',
     isa => Str,
     predicate => 1,
     init_arg => undef,
+    documentation => q{Since 0.1409, consider using 'distribution_name' in doctype:global instead.},
 );
 has author => (
     is => 'rw',
@@ -102,6 +104,9 @@ sub parse_header {
         if(!$self->$predicate && $value) {
             $self->$setting($value);
         }
+    }
+    if (!$self->has_name && $self->global && $self->global->has_distribution_name) {
+        $self->name($self->global->distribution_name);
     }
 }
 
